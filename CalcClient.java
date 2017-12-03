b3eac4297826803133cec6b3ae7f0112af509630
mskrzypk <mskrzypk@mion.elka.pw.edu.pl>
2017-12-03 20:35:40 +0100

    multi-threaded server that calculates simple numbers doubles them and sleeps for a few seconds to show multithreading

---
+++ CalcClient.java
@@ -0,0 +1,108 @@
+package Calculate;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalcClient {
+	
+    private	BufferedReader in;
+    private	PrintWriter out;
+	
+	JFrame frame = new JFrame("Chatter");
+    JTextField textField = new JTextField(40);
+    JTextArea messageArea = new JTextArea(8, 40);
+    
+    public CalcClient()
+    {
+    	textField.setEditable(false);
+    	messageArea.setEditable(false);
+    	frame.getContentPane().add(textField, "North");
+    	frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+    	frame.setSize(360, 480);
+    	
+    	textField.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				out.println(textField.getText());
+				textField.setText("");
+				
+			}
+		});
+    }
+    
+    private String getServerAdress() 
+    {
+    	return JOptionPane.showInputDialog(frame,  "Enter IP of the Server", "Welcome to the Client Chatter", JOptionPane.QUESTION_MESSAGE);
+    }
+    
+    private String getName()
+    {
+    	return JOptionPane.showInputDialog(frame,  "Enter your name", "Welcome to the Client Chatter", JOptionPane.PLAIN_MESSAGE);
+    }
+    
+    private void run() throws IOException
+    {
+    	String serverAddress = getServerAdress();
+    	
+    	Socket socket= new Socket(serverAddress, 9040);
+    	in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    	out = new PrintWriter(socket.getOutputStream(), true);
+    	
+    	while(true) 
+    	{
+    		String line= in.readLine();
+    		if(line.startsWith("SUMBITNAME"))
+    		{
+    			out.println(getName());
+    		}
+    		
+    		else if (line.startsWith("NAMEACCEPTED"))
+    		{
+    			textField.setEditable(true);
+    		}
+    		else if (line.startsWith("MESSAGE"))
+    		{
+    			messageArea.append(line.substring(8)+"\n");
+    		}
+    	}
+    }
+	
+	public static void main(String[] argv) throws Exception
+	{
+		 CalcClient client = new CalcClient();
+		 client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	     client.frame.setVisible(true);
+	     client.run();
+	     
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
+
---
+++ Calculate.java
@@ -0,0 +1,125 @@
+package Calculate;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+
+public class Calculate {
+	
+	private static final int PORT= 9040;
+	
+	private static HashSet<String> names= new HashSet<String>();
+	
+	private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+	
+	
+	
+	public static void main(String[] argv) throws Exception
+	{
+		System.out.println("The server is on");
+		
+		ServerSocket listener = new ServerSocket(PORT);
+		try
+		{
+			while (true)
+			{
+				new Handler(listener.accept()).start();
+			}
+		}
+		finally
+		{
+			listener.close();
+		}
+		
+	}
+	
+	private static class Handler extends Thread 
+	{
+		private String name;
+		private Socket socket;
+		private BufferedReader in;
+		private PrintWriter out;
+		
+		public Handler(Socket socket)
+		{
+			this.socket= socket;
+		}
+	
+	
+            public void run() 
+            {
+		try
+		{
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+			out = new PrintWriter(socket.getOutputStream(), true);
+			
+			while (true) //sprobowac zamienic na wait i notify
+			{
+				out.println("SUMBITNAME");
+				name= in.readLine();
+				if (name== null) 
+				{
+					return;
+				}
+				synchronized (names) 
+				{
+					if (!names.contains(name))
+					{
+						names.add(name);
+						break;   //sprobowac zamienic na break i notify
+					}
+				}
+			}
+			
+			out.println("NAMEACCEPTED");
+			writers.add(out);
+			
+			while(true)
+			{
+				String input=in.readLine();
+                                int temp = Integer.parseInt(input);
+				if(input==null)
+				{
+					return;
+				}
+                                sleep(2000);
+                                temp*=2;
+				for (PrintWriter writer : writers)
+				{
+                                    
+					writer.println("MESSAGE "+name+": "+temp);
+				}
+			}
+			
+		}
+		catch (IOException|InterruptedException e)
+		{
+			System.out.println(e);
+		}
+		finally
+		{
+			if (name != null)
+			{
+				names.remove(name);
+			}
+			if (out != null)
+			{
+				writers.remove(out);
+			}
+			try
+			{
+				socket.close();
+			}
+			catch(IOException e)
+			{}
+		}
+            }
+		
+		
+        }
+}
+	
